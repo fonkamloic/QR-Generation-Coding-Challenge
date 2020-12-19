bb379be4a776d3812848267dad22f5faa9d491e7
Fonkam Loïc <fonkamloic@gmail.com>
2020-12-19 14:28:07 +0100

    injection_init_file

---
+++ lib/injection.dart
@@ -0,0 +1,16 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+
+import 'injection.config.dart';
+
+final GetIt getIt = GetIt.instance;
+
+@injectableInit
+void configureInjection(String env) async => await $initGetIt(getIt, environment: env);
+
+abstract class Env {
+  static const development = 'development';
+  static const production = 'production';
+  static const staging = 'staging';
+}
