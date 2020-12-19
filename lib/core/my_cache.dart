bfa1527c05575f180f015c41a454b198facf9da9
Fonkam Loïc <fonkamloic@gmail.com>
2020-12-19 14:24:11 +0100

    injectable modules and Persistence storage

---
+++ lib/core/my_cache.dart
@@ -0,0 +1,18 @@
+import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'injectable_modules.dart';
+
+const CACHED_SEED = "CACHED_SEED";
+
+@lazySingleton
+class Persistence {
+  final SharedPreferences _preferences;
+
+  Persistence(this._preferences);
+
+  // set seed
+  Future<void> setSeed(String value) async =>
+      _preferences.setString(CACHED_SEED, value);
+  // getSeed
+  String get getCachedSeed => _preferences.getString(CACHED_SEED ?? "No value in Cache");
+}
