d99aba5db19bb6b195473be0395fa23e1f4a142d
Fonkam Loïc <fonkamloic@gmail.com>
2020-12-19 14:40:50 +0100

    qr code generation screen

--- lib/qr_code_screen.dart
+++ lib/qr_code_screen.dart
@@ -1,12 +1,11 @@
-import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_countdown_timer/index.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:qr_challenge/application/send_gen_bloc/seed_gen_bloc.dart';
-import 'package:qr_challenge/core/my_cache.dart';
-import 'package:qr_challenge/injection.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+
+import 'application/send_gen_bloc/seed_gen_bloc.dart';
+import 'core/my_cache.dart';
+import 'injection.dart';
 
 class QRCodeScreen extends StatelessWidget {
   @override
