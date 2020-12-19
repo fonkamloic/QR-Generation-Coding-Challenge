ac75cb3ffd4a6a6c9f70056c562b78b422b03d8c
Fonkam Loïc <fonkamloic@gmail.com>
2020-12-19 14:26:11 +0100

    seed model with primitive datatype and seed repository

---
+++ lib/infrastructure/send_gen/seed_dtos.dart
@@ -0,0 +1,27 @@
+import 'package:chopper/chopper.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:qr_challenge/domain/seed.dart';
+
+part 'seed_dtos.freezed.dart';
+
+@freezed
+abstract class SeedDto implements _$SeedDto {
+  const SeedDto._();
+  const factory SeedDto({
+    @required String value,
+    @required DateTime expires_at,
+  }) = _SeedDto;
+
+  Seed toDomain() {
+    return Seed(
+        value: value,
+        timeLeft:
+            Duration(seconds: expires_at.difference(DateTime.now()).inSeconds));
+  }
+
+  factory SeedDto.fromResponse(Response response) => SeedDto(
+      value: response.body['seed'] as String,
+      expires_at: DateTime.fromMillisecondsSinceEpoch(
+          response.body['expires_at'] as int));
+}
